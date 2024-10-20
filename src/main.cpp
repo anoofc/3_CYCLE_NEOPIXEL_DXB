49ca6e7af22fe2fe92e2fd378a6dc0fa1fb8e43f
Anoof Chappangathil <anoof7262@gmail.com>
2024-10-20 15:07:28 +0400

    Refactor strip initialization and update LED strip lengths

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,9 @@
 #define LED_2         26
 #define LED_3         25
 
-#define STRIP1_NUMPIXELS     70
-#define STRIP2_NUMPIXELS     70
-#define STRIP3_NUMPIXELS     70
+#define STRIP1_NUMPIXELS     300
+#define STRIP2_NUMPIXELS     300
+#define STRIP3_NUMPIXELS     300
 
 #define TIMOUT_1             15000
 #define TIMOUT_2             15000
@@ -32,10 +32,6 @@
 #include <Adafruit_NeoPixel.h>
 #include "BluetoothSerial.h"
 
-bool      cylcle1_active         = false;
-bool      cylcle2_active         = false;
-bool      cylcle3_active         = false;
-
 bool      led1Blink              = false;
 bool      led2Blink              = false;
 bool      led3Blink              = false;
@@ -44,24 +40,20 @@ bool      sens1LastState         = false;
 bool      sens2LastState         = false;
 bool      sens3LastState         = false;
 
-uint8_t   cycleOneCounter        = 0;
-uint8_t   cycleTwoCounter        = 0;
-uint8_t   cycleThreeCounter      = 0;
+uint16_t   cycleOneCounter        = 0;
+uint16_t   cycleTwoCounter        = 0;
+uint16_t   cycleThreeCounter      = 0;
 
 uint32_t  cycleOneUpdateMillis   = 0;
 uint32_t  cycleTwoUpdateMillis   = 0;
 uint32_t  cycleThreeUpdateMillis = 0;
 
-uint32_t  cycleOneTimer          = 0;
-uint32_t  cycleTwoTimer          = 0;
-uint32_t  cycleThreeTimer        = 0;
-
 
 BluetoothSerial SerialBT;
 
-Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(STRIP1_NUMPIXELS, LED_1, NEO_BRG + NEO_KHZ800);
-Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(STRIP2_NUMPIXELS, LED_2, NEO_BRG + NEO_KHZ800);
-Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(STRIP3_NUMPIXELS, LED_3, NEO_BRG + NEO_KHZ800); 
+Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(STRIP1_NUMPIXELS, LED_1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(STRIP2_NUMPIXELS, LED_2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip3 = Adafruit_NeoPixel(STRIP3_NUMPIXELS, LED_3, NEO_GRB + NEO_KHZ800); 
 
 void stripTest(){
   for(int i=0; i<STRIP1_NUMPIXELS; i++){
@@ -99,7 +91,7 @@ void blinkLED1() {
       ledState = !ledState;
 
       if (ledState) {
-        for (uint8_t i = 0; i < STRIP1_NUMPIXELS; i++) {
+        for (uint16_t i = 0; i < STRIP1_NUMPIXELS; i++) {
           strip1.setPixelColor(i, RED);
         }
       } else {
@@ -128,7 +120,7 @@ void blinkLED2() {
       ledState = !ledState;
 
       if (ledState) {
-        for (uint8_t i = 0; i < STRIP2_NUMPIXELS; i++) {
+        for (uint16_t i = 0; i < STRIP2_NUMPIXELS; i++) {
           strip2.setPixelColor(i, BLUE);
         }
       } else {
@@ -157,7 +149,7 @@ void blinkLED3() {
       ledState = !ledState;
 
       if (ledState) {
-        for (uint8_t i = 0; i < STRIP3_NUMPIXELS; i++) {
+        for (uint16_t i = 0; i < STRIP3_NUMPIXELS; i++) {
           strip3.setPixelColor(i, PURPLE);
         }
       } else {
@@ -178,15 +170,13 @@ void cycle1Handler(){
   if (millis() - cycleOneUpdateMillis < TIMOUT_1){ return; }
   if (SENSOR_1_DETECTED && sens1LastState){
     Serial.println("A_1");
-    for (int i=0; i<cycleOneCounter; i++){
-      strip1.setPixelColor(i, RED);
-    }
+    strip1.setPixelColor(cycleOneCounter, RED);
     strip1.show();
     cycleOneCounter++;
     if (cycleOneCounter == STRIP1_NUMPIXELS){
       Serial.println("A_0");
-      cycleOneCounter = 0;
       led1Blink = true;
+      cycleOneCounter = 0;
       cycleOneUpdateMillis = millis();
     }
   } 
@@ -196,15 +186,13 @@ void cycle2Handler(){
   if (millis() - cycleTwoUpdateMillis < TIMOUT_2){ return; }
   if (SENSOR_2_DETECTED && sens2LastState){
     Serial.println("B_1");
-    for (int i=0; i<cycleTwoCounter; i++){
-      strip2.setPixelColor(i, BLUE);
-    }
+    strip2.setPixelColor(cycleTwoCounter, BLUE);
     strip2.show();
     cycleTwoCounter++;
     if (cycleTwoCounter == STRIP2_NUMPIXELS){
       Serial.println("B_0");
-      cycleTwoCounter = 0;
       led2Blink = true;
+      cycleTwoCounter = 0;
       cycleTwoUpdateMillis = millis();
     }
   } 
@@ -214,15 +202,13 @@ void cycle3Handler(){
   if (millis() - cycleThreeUpdateMillis < TIMOUT_3){ return; }
   if (SENSOR_3_DETECTED && sens3LastState){
     Serial.println("C_1");
-    for (int i=0; i<cycleThreeCounter; i++){
-      strip3.setPixelColor(i, PURPLE);
-    }
+    strip3.setPixelColor(cycleThreeCounter, PURPLE);
     strip3.show();
     cycleThreeCounter++;
     if (cycleThreeCounter == STRIP3_NUMPIXELS){
       Serial.println("C_0");
-      cycleThreeCounter = 0;
       led3Blink = true;
+      cycleThreeCounter = 0;
       cycleThreeUpdateMillis = millis();
     }
   } 
@@ -248,6 +234,13 @@ void processData(char data){
     led2Blink = true;
   } else if (data == 'C'){
     led3Blink = true;
+  } else if (data == 'Z'){
+    strip1.clear(); strip1.show();
+    strip2.clear(); strip2.show();
+    strip3.clear(); strip3.show();
+    cycleOneCounter   = 0;  cycleOneUpdateMillis   = millis();  Serial.println("A_0");
+    cycleTwoCounter   = 0;  cycleTwoUpdateMillis   = millis();  Serial.println("B_0");
+    cycleThreeCounter = 0;  cycleThreeUpdateMillis = millis();  Serial.println("C_0");
   }
   // TODO: Implement this function
 }
@@ -302,7 +295,7 @@ void loop() {
 
   updateSensorState();
 
-  readBTSerial();
-  // readSerial();
+  // readBTSerial();
+  readSerial();
 }
 
